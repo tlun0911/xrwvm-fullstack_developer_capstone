a42eca8ef7a62435b207a8590a05a98623dd5712
tlun0911 <tlun0911@gmail.com>
2024-04-30 18:30:48 -0400

    Updates

--- server/database/app.js
+++ server/database/app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const fs = require("fs");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3030;
 
 app.use(cors());
-app.use(require("body-parser").urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", "utf8"));
 const dealerships_data = JSON.parse(
@@ -21,10 +22,10 @@ const Dealerships = require("./dealership");
 
 try {
   Reviews.deleteMany({}).then(() => {
-    Reviews.insertMany(reviews_data["reviews"]);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(() => {
-    Dealerships.insertMany(dealerships_data["dealerships"]);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
 } catch (error) {
   res.status(500).json({ error: "Error fetching documents" });
