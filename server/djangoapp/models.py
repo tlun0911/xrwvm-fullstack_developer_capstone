319ca54e44ab946f49a5c5b17858b5c75e1e735a
tlun0911 <tlun0911@gmail.com>
2024-05-01 10:45:18 -0400

    new updates

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -13,6 +13,14 @@
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    domestic = models.BooleanField(default=True, blank=True)
+
+    def __str__(self):
+        return self.name
+        
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +31,24 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CHOICE_TYPE = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        ('CROSSOVER', 'Crossover'),
+        ('VAN', 'Van'),
+    ]
+    type = models.CharField(max_length=100, choices=CHOICE_TYPE, default='SEDAN')
+    year = models.IntegerField(default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)])
+    electric = models.BooleanField(default=False)
+    used = models.BooleanField(default=False)
+    miles = models.IntegerField(default=0, blank=True)
+
+    def __str__(self):
+        return self.name
+
